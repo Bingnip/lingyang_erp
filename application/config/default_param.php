407903f9563e624f86b1fee06de4bd32cf0483cd
Clementine <406584573@qq.com>
2020-03-04 19:47:37 +0800

    !

--- application/config/default_param.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config = [
-    'default_alert_num' => 1,  //默认预警库存
-    'default_buy_num' => 1, //默认最低购买数
-    'default_ini_inventory' => 1,
-    'finishedProduct' => 12,  //成品（独立商品）
-    'semiFinishedProduct' => 13,  //半成品（配件商品）
-];
